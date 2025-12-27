1aeb5c3d2d11564328299e4983c3c1db9b0bffef
Legjones <Legjones@users.noreply.github.com>
2025-12-27 23:35:19 +0000

    Deploying to gh-pages from @ Legjones/Legjones.github.io@5950cd38494fbbfb73ed9135da085e3b633b797d 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "Legjones/PersonalWebsite",
+    "data-repo": "Legjones/Legjones.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
